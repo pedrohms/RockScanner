249765b95e5f043bf99fced4bacf03085189b63b
Pedro Souza <pedrohms412@gmail.com>
2025-06-03 15:47:43 -0300

    Testing

--- src/main/java/org/pessoal/rockscanner/events/CustomEvents.java
+++ src/main/java/org/pessoal/rockscanner/events/CustomEvents.java
@@ -38,81 +38,83 @@ public class CustomEvents {
 
     @SubscribeEvent
     public static void OnRightClick(PlayerInteractEvent.RightClickBlock playerInteractEvent) {
-        if (ModList.get().isLoaded("journeymap") && ModList.get().isLoaded("gtlib")) {
-            Player player = playerInteractEvent.getPlayer();
-            if (player.isLocalPlayer()) return;
-            if (playerInteractEvent.getHand() == InteractionHand.OFF_HAND) return;
-            BlockState state = playerInteractEvent.getWorld().getBlockState(playerInteractEvent.getPos());
-            Item item = Item.BY_BLOCK.get(state.getBlock());
+        try {
+            if (ModList.get().isLoaded("journeymap") && ModList.get().isLoaded("gtlib")) {
+                Player player = playerInteractEvent.getPlayer();
+                if (player.isLocalPlayer()) return;
+                if (playerInteractEvent.getHand() == InteractionHand.OFF_HAND) return;
+                BlockState state = playerInteractEvent.getWorld().getBlockState(playerInteractEvent.getPos());
+                Item item = Item.BY_BLOCK.get(state.getBlock());
 //        SendMessage(player, "1 - " + item.asItem().getDescription().getString());
 //        SendMessage(player, "2 - " + item.toString());
 //        SendMessage(player, "3 - " + item.asItem().toString());
 //        SendMessage(player, "4 - " + state.getBlock().toString());
-            if (state.getBlock() instanceof BlockSurfaceRock surfaceRock) {
+                if (state.getBlock() instanceof BlockSurfaceRock surfaceRock) {
 //            SendMessage(player, "5 - " + surfaceRock.getStoneType().getMaterial().toString());
 //            SendMessage(player, "5 - " + surfaceRock.toString());
 //            SendMessage(player, "5.1 - " + surfaceRock.getMaterial());
 
 
-                if (!surfaceRock.asItem().toString().contains("surface_rock")) return;
+                    if (!surfaceRock.asItem().toString().contains("surface_rock")) return;
 
-                if (!item.asItem().getDescription().getString().endsWith("Surface Rock")) return;
+                    if (!item.asItem().getDescription().getString().endsWith("Surface Rock")) return;
 
-                String str = item.asItem().getDescription().getString();
-                final String ROCK = "Surface Rock";
-                try {
-                    str = str.substring(0, str.indexOf(ROCK) - 1).trim();
-                } catch (Exception e) {
-                    return;
-                }
-                StringBuilder vein_string = new StringBuilder(str.substring(str.lastIndexOf(" ")).trim());
+                    String str = item.asItem().getDescription().getString();
+                    final String ROCK = "Surface Rock";
+                    try {
+                        str = str.substring(0, str.indexOf(ROCK) - 1).trim();
+                    } catch (Exception e) {
+                        return;
+                    }
+                    StringBuilder vein_string = new StringBuilder(str.substring(str.lastIndexOf(" ")).trim());
 
-                if (JourneyImpl.CLIENT_API != null) {
-                    LevelChunk chunk = playerInteractEvent.getWorld().getChunkAt(playerInteractEvent.getPos());
-                    if (item.asItem().getDescription().getString().split(" ").length > 3) {
+                    if (JourneyImpl.CLIENT_API != null) {
+                        LevelChunk chunk = playerInteractEvent.getWorld().getChunkAt(playerInteractEvent.getPos());
+                        if (item.asItem().getDescription().getString().split(" ").length > 3) {
 
-                        String materialChunkName = "";
-                        String seedId = "";
-                        Map<Material, LongList> oresInChunk = RockScannerMod.getVeinSavedData().geOresInChunk(playerInteractEvent.getPos().getX() >> 4, playerInteractEvent.getPos().getZ() >> 4);
-                        AtomicReference<ChunkPos> chunkTemp = new AtomicReference<>();
-                        List<Map.Entry<Material, LongList>> top4 = new ArrayList<>();
-                        if( oresInChunk != null && !oresInChunk.isEmpty()) {
-                            top4 = oresInChunk.entrySet().stream().sorted((o1, o2) -> Integer.compare(o2.getValue().size(), o1.getValue().size()))
-                                    .limit(4)
-                                    .toList();
-                        }
+                            String materialChunkName = "";
+                            String seedId = "";
+                            Map<Material, LongList> oresInChunk = RockScannerMod.getVeinSavedData().geOresInChunk(playerInteractEvent.getPos().getX() >> 4, playerInteractEvent.getPos().getZ() >> 4);
+                            AtomicReference<ChunkPos> chunkTemp = new AtomicReference<>();
+                            List<Map.Entry<Material, LongList>> top4 = new ArrayList<>();
+                            if (oresInChunk != null && !oresInChunk.isEmpty()) {
+                                top4 = oresInChunk.entrySet().stream().sorted((o1, o2) -> Integer.compare(o2.getValue().size(), o1.getValue().size()))
+                                        .limit(4)
+                                        .toList();
+                            }
 
 //                        Vein vein = null;
-                        ChunkPos finalChunk = chunk.getPos();
-                        if(chunkTemp.get() != null){
-                            finalChunk = chunkTemp.get();
-                        }
+                            ChunkPos finalChunk = chunk.getPos();
+                            if (chunkTemp.get() != null) {
+                                finalChunk = chunkTemp.get();
+                            }
 
-                        StringBuilder oreVeinList = new StringBuilder(vein_string.toString());
-                        for(Map.Entry<Material,LongList> ores : top4){
-                            oreVeinList.append("\n").append(ores.getKey().getId());
-                        }
-                        final ChunkPos f = finalChunk;
-                        List<Waypoint> waypointList = JourneyImpl.CLIENT_API.getAllWaypoints().stream().filter(waypoint -> {
-                            if(waypoint.getPosition().getX() >= f.getMinBlockX() - (GTLibConfig.ORE_VEIN_MAX_SIZE.get() ) &&
-                                    waypoint.getPosition().getX() <= f.getMaxBlockX() + (GTLibConfig.ORE_VEIN_MAX_SIZE.get() ) &&
-                                waypoint.getPosition().getZ() >= f.getMinBlockZ() - (GTLibConfig.ORE_VEIN_MAX_SIZE.get() ) &&
-                                    waypoint.getPosition().getZ() <= f.getMaxBlockZ() + (GTLibConfig.ORE_VEIN_MAX_SIZE.get() )){
+                            StringBuilder oreVeinList = new StringBuilder(vein_string.toString());
+                            for (Map.Entry<Material, LongList> ores : top4) {
+                                oreVeinList.append("\n").append(ores.getKey().getId());
+                            }
+                            final ChunkPos f = finalChunk;
+                            List<Waypoint> waypointList = JourneyImpl.CLIENT_API.getAllWaypoints().stream().filter(waypoint -> {
+                                if (waypoint.getPosition().getX() >= f.getMinBlockX() - (GTLibConfig.ORE_VEIN_MAX_SIZE.get()) &&
+                                        waypoint.getPosition().getX() <= f.getMaxBlockX() + (GTLibConfig.ORE_VEIN_MAX_SIZE.get()) &&
+                                        waypoint.getPosition().getZ() >= f.getMinBlockZ() - (GTLibConfig.ORE_VEIN_MAX_SIZE.get()) &&
+                                        waypoint.getPosition().getZ() <= f.getMaxBlockZ() + (GTLibConfig.ORE_VEIN_MAX_SIZE.get())) {
                                     return true;
+                                }
+                                return false;
+                            }).toList();
+
+                            if (!waypointList.isEmpty()) return;
+                            if (!top4.isEmpty() && top4.stream().toList().get(0).getValue().size() < 200) return;
+
+                            for (Map.Entry<Material, LongList> ores : top4) {
+                                SendMessage(player, "Ore: " + ores.getKey().getId() + " - " + ores.getValue().size());
                             }
-                            return false;
-                        }).toList();
 
-                        if (!waypointList.isEmpty()) return;
-
-                            for(Map.Entry<Material, LongList> ores : top4 ){
-                                SendMessage(player, "Ore: " + ores.getKey().getId() );
-                            }
-
-                        Random rdn = new Random();
-                        int r = rdn.nextInt(256);
-                        int g = rdn.nextInt(256);
-                        int b = rdn.nextInt(256);
+                            Random rdn = new Random();
+                            int r = rdn.nextInt(256);
+                            int g = rdn.nextInt(256);
+                            int b = rdn.nextInt(256);
 //                        MarkerOverlay overlay = MarkerOverlayFactory.create(
 //                                JourneyImpl.CLIENT_API,
 //                                vein_string.toString(),
@@ -123,21 +125,23 @@ public class CustomEvents {
 //                        );
 //                        RockScannerMod.getOverlayData().addOverlay(player, overlay);
 
-                        WaypointGroup group = new WaypointGroup(RockScannerMod.MODID, "OreVeins");
+                            WaypointGroup group = new WaypointGroup(RockScannerMod.MODID, "OreVeins");
 
 
-                        Waypoint wDirt = new Waypoint(RockScannerMod.MODID, vein_string.toString(), player.getLevel().dimension(), playerInteractEvent.getPos());
-                        wDirt.setColor(new Color(r, g, b).getRGB());
-                        wDirt.setGroup(group);
-                        try {
+                            Waypoint wDirt = new Waypoint(RockScannerMod.MODID, vein_string.toString(), player.getLevel().dimension(), playerInteractEvent.getPos());
+                            wDirt.setColor(new Color(r, g, b).getRGB());
+                            wDirt.setGroup(group);
+                            try {
 //                            JourneyImpl.CLIENT_API.show(overlay);
-                            JourneyImpl.CLIENT_API.show(wDirt);
-                        } catch (Exception e) {
-                            RockScannerMod.LOGGER.error("{0}", e);
+                                JourneyImpl.CLIENT_API.show(wDirt);
+                            } catch (Exception e) {
+                                RockScannerMod.LOGGER.error("{0}", e);
+                            }
                         }
                     }
                 }
             }
+        } catch (Exception ex) {
         }
 
     }
